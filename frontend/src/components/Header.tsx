add9d1272febb758d8b09a6eb40fbc995b78f454
fabian <fabian.sucholas123@gmail.com>
2025-11-07 12:11:49 +0100

    header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,13 +4,30 @@ interface HeaderProps {
   onShowUsageInfo: () => void;
 }
 
-export function Header({ onShowHexOutput, onShowSyscalls, onShowUsageInfo }: HeaderProps) {
+export function Header({
+  onShowHexOutput,
+  onShowSyscalls,
+  onShowUsageInfo,
+}: HeaderProps) {
   return (
     <header className="bg-gradient-to-r from-blue-600 to-blue-700 shadow-lg">
       <div className="max-w-[1800px] mx-auto px-6 py-4 flex items-center justify-between">
         <div className="flex items-center gap-3">
           <div>
-            <h1 className="text-2xl font-bold text-white">8051 Emulator with support for DSM-51 instructions</h1>
+            <h1 className="text-2xl font-bold text-white">
+              8051 Emulator with support for DSM-51 instructions
+            </h1>
+            <span className="text-sm text-white">
+              Mostyly works, but there are still shit ton of bugs&nbsp;-&nbsp;
+              <a
+                href="https://github.com/Fejbien/8051emu"
+                className="text-sm text-white hover:text-blue-300 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub repo
+              </a>
+            </span>
           </div>
         </div>
         <div className="flex gap-3">
